6986a1b4e1b58af7371a2beb4ab810e0e0c8d699
hp15f <hp15f@LAPTOP-1G0PTJV1>
2024-11-26 07:52:31 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.Serialization;
+
+int currentHours = 23;
+int currentMinutes = 59;
+int minutesToAdd = 2;
+DateTime currentTime = new DateTime(1, 1, 1, currentHours, currentMinutes, 0);
+DateTime newTime = currentTime.AddMinutes(minutesToAdd);
+Console.WriteLine(newTime.ToString("HH:mm"));
